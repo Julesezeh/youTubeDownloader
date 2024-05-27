36ac5faa13aa976fef673a22434a9d33e1b69324
Jules Okoye-ezeh <julesezeh23@gmail.com>
2024-05-27 18:25:37 +0100

    function to get streams ( thumbnail for now)

--- YouTubeDownloader/views.py
+++ YouTubeDownloader/views.py
@@ -9,3 +9,13 @@ def index(request):
         stream.download()
         return render(request,"index.html")
     return render(request,"index.html")
+
+def get_streams(request):
+    if request.method == "POST":
+        print("I was called")
+        link =  request.POST.get("link")
+        you_tube = YouTube(link)
+        # stream = you_tube.streams.get_highest_resolution()
+        stream_image_url = you_tube.thumbnail_url
+
+        return render(request, 'partials/get-streams.html',{'stream_image':stream_image_url})
