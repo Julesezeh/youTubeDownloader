2f5f2908a6f370b44ce65ee7efd498a687246a92
Jules Okoye-ezeh <julesezeh23@gmail.com>
2024-05-30 13:37:57 +0100

    generating and passing the appropriate data for the target template

--- YouTubeDownloader/views.py
+++ YouTubeDownloader/views.py
@@ -12,10 +12,25 @@ def index(request):
 
 def get_streams(request):
     if request.method == "POST":
-        print("I was called")
-        link =  request.POST.get("link")
+        link = request.POST.get("link")
         you_tube = YouTube(link)
-        # stream = you_tube.streams.get_highest_resolution()
-        stream_image_url = you_tube.thumbnail_url
-
-        return render(request, 'partials/get-streams.html',{'stream_image':stream_image_url})
+        
+        # Get the thumbnail URL
+        thumbnail_url = you_tube.thumbnail_url
+        
+        # Get all the video streams with their download URLs
+        video_streams = you_tube.streams.filter(progressive=True, file_extension='mp4')
+        download_links = []
+        for stream in video_streams:
+            download_links.append({
+                "resolution": stream.resolution,
+                "mime_type": stream.mime_type,
+                "url": stream.url
+            })
+        
+        # Pass the thumbnail URL and download URLs to the template
+        return render(request, "partials/get-streams.html", {
+            "stream_image": thumbnail_url,
+            "download_links": download_links,
+            "video_title": you_tube.title,
+        })
